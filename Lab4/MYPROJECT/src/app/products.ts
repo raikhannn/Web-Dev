f6833a67d102758c910540d59c9c729191e60a32
admin <r_ubaidullayeva@kbtu.kz>
2024-02-22 23:20:17 +0600

    Add files

---
+++ Lab4/MYPROJECT/src/app/products.ts
@@ -0,0 +1,100 @@
+export interface Product {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+    link: string;
+    imageUrl: string;
+  }
+  
+  export const products = [
+    {
+      id: 1,
+      name: 'Apple iPhone 15 Pro',
+      price: 568990,
+      description: 
+      'A phone that girls need to take the best photos ever',
+      link: 'https://kaspi.kz/shop/p/apple-iphone-15-pro-256gb-seryi-113138213/?c=750000000',
+      imageUrl: 'https://resources.cdn-kaspi.kz/img/m/p/hae/h00/83559620739102.jpg?format=preview-large'
+    },
+    {
+      id: 2,
+      name: 'Dyson HS05 Airwrap Complete Long Styler 1300 W',
+      price: 268000,
+      description: 'The baddest Hair Essential.',
+      link: 'https://kaspi.kz/shop/p/dyson-hs05-airwrap-complete-long-stailer-1300-w-104498649/?c=750000000',
+      imageUrl: 'https://resources.cdn-kaspi.kz/img/m/p/h17/hd9/82802441289758.jpg?format=preview-large'
+    },
+    {
+      id: 3,
+      name: 'Charlotte Tilbury Blush And Glow Collection',
+      price: 32180,
+      description: 'Shine bright like a Diamond! (especially on the one of those parties)',
+      link: 'https://kaspi.kz/shop/p/charlotte-tilbury-blush-and-glow-nabor-dekorativnoi-kosmetiki-dlja-zhenschin-113390208/?c=750000000',
+      imageUrl: 'https://resources.cdn-kaspi.kz/img/m/p/h01/h5b/83865709412382.jpg?format=preview-large'
+    },
+    {
+      id: 4,
+      name: 'Maison Francis Kurkdjian Baccarat Rouge 540 EDP 70 ml., unisex',
+      price: 134000,
+      description: 'No words. Just hear it.',
+      link: 'https://kaspi.kz/shop/p/maison-francis-kurkdjian-baccarat-rouge-540-parfjumernaja-voda-edp-70-ml-uniseks-17302860/?c=750000000',
+      imageUrl: 'https://resources.cdn-kaspi.kz/img/m/p/hd1/ha9/85209578176542.png?format=preview-large'
+    },
+    {
+      id: 5,
+      name: 'Luni The Styler',
+      price: 7000,
+      description: 'This is a pink girly styler for those who cant afford buying a Dyson. Nevertheless, this Luni thing will handle your gorgeous hair as well as its big sister!',
+      link: 'https://kaspi.kz/shop/p/luni-dlja-lokonov-ploika-115715372/?c=750000000',
+      imageUrl: 'https://resources.cdn-kaspi.kz/img/m/p/h68/h1e/84887153573918.jpg?format=preview-large'
+    },
+    {
+      id: 6,
+      name: 'MONTALE Chocolate Greedy EDP 100 ml., unisex',
+      price: 55000,
+      description: 'Do you want people around you to think that you were born in the chocolate & cookies factory? Order Chocolate Greedy, spill it on yourself and check!',
+      link: 'https://kaspi.kz/shop/p/montale-chocolate-greedy-parfjumernaja-voda-edp-100-ml-uniseks-17300734/?c=750000000',
+      imageUrl: 'https://resources.cdn-kaspi.kz/img/m/p/hca/ha8/63777205911582.jpg?format=preview-large'
+    },
+    {
+      id: 7,
+      name: 'Rhode Peptide Lip Treatment salted caramel Balm | 10 ml.',
+      price: 14700,
+      description: 'The baddest balm ever! Keeps your lips very soft and VERY hydrated! Also, it tastes like latte with salted caramel syrup <3',
+      link: 'https://kaspi.kz/shop/p/rhode-bal-zam-peptide-lip-treatment-salted-caramel-10-ml-112519613/?c=750000000',
+      imageUrl: 'https://resources.cdn-kaspi.kz/img/m/p/h32/h54/82798277820446.jpg?format=preview-large'
+    },
+    {
+      id: 8,
+      name: 'Apple AirPods Max | Green',
+      price: 276000,
+      description: 'Wanna listen all the basses from your favourite song? AirPods on the Way rn! Moreover, the pods are GREEN so it is the trendy color!',
+      link: 'https://kaspi.kz/shop/p/apple-airpods-max-zelenyi-101161547/?c=750000000',
+      imageUrl: 'https://resources.cdn-kaspi.kz/img/m/p/hca/ha8/63777205911582.jpg?format=preview-large'
+    },
+    {
+      id: 9,
+      name: 'Dior Lip Glow Oil Pink Lip Balm 007',
+      price: 20890,
+      description: 'Nowadays, Lip Glow Oil is a MUST in lip care.',
+      link: 'https://kaspi.kz/shop/p/dior-addict-lip-glow-oil-blesk-dlja-gub-rozovyi-prozrachnyi-015-106894426/?c=750000000',
+      imageUrl: 'https://resources.cdn-kaspi.kz/img/m/p/h45/ha4/83345807704094.jpg?format=preview-large'
+    },
+    {
+      id: 10,
+      name: 'Hempz Original body & hand cream | 500 ml.',
+      price: 14990,
+      description: 'Being made with using a cannabis seed, Hempz body cream keeps the position of the most moisturising cream ever!',
+      link: 'https://kaspi.kz/shop/p/hempz-original-krem-dlja-tela-500-ml-101346239/?c=750000000',
+      imageUrl: 'https://resources.cdn-kaspi.kz/img/m/p/hea/hc1/64110916141086.jpg?format=preview-large'
+    },
+  
+  ];
+  
+  
+  /*
+  Copyright Google LLC. All Rights Reserved.
+  Use of this source code is governed by an MIT-style license that
+  can be found in the LICENSE file at https://angular.io/license
+  */
